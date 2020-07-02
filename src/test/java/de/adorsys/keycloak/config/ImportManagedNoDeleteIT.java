7c11bf309f1a295145a2b61afda4cca666b25065
Ramon Spahr <spahr@puzzle.ch>
2020-07-02 14:32:39 +0200

    improve identity provider handling (managed, delete, incl. mappers)

--- src/test/java/de/adorsys/keycloak/config/ImportManagedNoDeleteIT.java
+++ src/test/java/de/adorsys/keycloak/config/ImportManagedNoDeleteIT.java
@@ -33,13 +33,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 
 @TestPropertySource(properties = {
-        "import.managed.authentication-flow=no-delete",
-        "import.managed.group=no-delete",
-        "import.managed.required-action=no-delete",
-        "import.managed.client-scope=no-delete",
-        "import.managed.scope-mapping=no-delete",
-        "import.managed.component=no-delete",
-        "import.managed.sub-component=no-delete",
+    "import.managed.authentication-flow=no-delete",
+    "import.managed.group=no-delete",
+    "import.managed.required-action=no-delete",
+    "import.managed.client-scope=no-delete",
+    "import.managed.scope-mapping=no-delete",
+    "import.managed.component=no-delete",
+    "import.managed.sub-component=no-delete",
+    "import.managed.identity-provider=no-delete",
+    "import.managed.identity-provider-mapper=no-delete",
 })
 class ImportManagedNoDeleteIT extends AbstractImportTest {
     private static final String REALM_NAME = "realmWithNoDelete";
@@ -79,38 +81,52 @@ class ImportManagedNoDeleteIT extends AbstractImportTest {
         assertThat(createdGroup, hasSize(2));
 
         List<RequiredActionProviderRepresentation> createdRequiredActions = createdRealm.getRequiredActions()
-                .stream()
-                .filter((action) -> action.getAlias().equals("MY_CONFIGURE_TOTP") || action.getAlias().equals("my_terms_and_conditions"))
-                .collect(Collectors.toList());
+            .stream()
+            .filter((action) -> action.getAlias().equals("MY_CONFIGURE_TOTP") || action.getAlias().equals("my_terms_and_conditions"))
+            .collect(Collectors.toList());
         assertThat(createdRequiredActions, hasSize(2));
 
         List<ClientScopeRepresentation> createdClientScopes = createdRealm.getClientScopes()
-                .stream()
-                .filter((clientScope) -> clientScope.getName().equals("my_clientScope") || clientScope.getName().equals("my_other_clientScope"))
-                .collect(Collectors.toList());
+            .stream()
+            .filter((clientScope) -> clientScope.getName().equals("my_clientScope") || clientScope.getName().equals("my_other_clientScope"))
+            .collect(Collectors.toList());
         assertThat(createdClientScopes, hasSize(2));
 
         List<ScopeMappingRepresentation> createdScopeMappings = createdRealm.getScopeMappings()
-                .stream()
-                .filter((scopeMapping) -> scopeMapping.getClientScope().equals("offline_access"))
-                .collect(Collectors.toList());
+            .stream()
+            .filter((scopeMapping) -> scopeMapping.getClientScope().equals("offline_access"))
+            .collect(Collectors.toList());
         assertThat(createdScopeMappings, hasSize(1));
 
         List<ComponentExportRepresentation> createdComponents = createdRealm.getComponents().get("org.keycloak.storage.UserStorageProvider")
-                .stream()
-                .filter(c -> c.getName().equals("my-realm-userstorage"))
-                .collect(Collectors.toList());
+            .stream()
+            .filter(c -> c.getName().equals("my-realm-userstorage"))
+            .collect(Collectors.toList());
         assertThat(createdComponents, hasSize(1));
 
         List<ComponentExportRepresentation> createdSubComponents = createdComponents.get(0)
-                .getSubComponents().getList("org.keycloak.storage.ldap.mappers.LDAPStorageMapper");
+            .getSubComponents().getList("org.keycloak.storage.ldap.mappers.LDAPStorageMapper");
         assertThat(createdSubComponents, hasSize(10));
 
         List<String> authenticationFlowsList = Arrays.asList("my auth flow", "my registration", "my registration form");
         List<AuthenticationFlowRepresentation> createdAuthenticationFlows = createdRealm.getAuthenticationFlows()
-                .stream()
-                .filter((authenticationFlow) -> authenticationFlowsList.contains(authenticationFlow.getAlias()))
-                .collect(Collectors.toList());
+            .stream()
+            .filter((authenticationFlow) -> authenticationFlowsList.contains(authenticationFlow.getAlias()))
+            .collect(Collectors.toList());
         assertThat(createdAuthenticationFlows, hasSize(3));
+
+        List<String> identityProviderList = Arrays.asList("my-first-idp", "my-second-idp");
+        List<IdentityProviderRepresentation> createdIdentityProviders = createdRealm.getIdentityProviders()
+            .stream()
+            .filter((identityProvider) -> identityProviderList.contains(identityProvider.getAlias()))
+            .collect(Collectors.toList());
+        assertThat(createdIdentityProviders, hasSize(2));
+
+        List<String> identityProviderMapperList = Arrays.asList("my-first-idp-mapper", "my-second-idp-mapper");
+        List<IdentityProviderMapperRepresentation> createdIdentityProviderMappers = createdRealm.getIdentityProviderMappers()
+            .stream()
+            .filter((identityProviderMapper) -> identityProviderMapperList.contains(identityProviderMapper.getName()))
+            .collect(Collectors.toList());
+        assertThat(createdIdentityProviderMappers, hasSize(2));
     }
 }
